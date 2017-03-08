6d93e8fc881c2562dbc8909e73c2b593dcdb7420
Edson Zappe <Edson Zappe>
2017-03-08 09:52:03 -0300

    ...

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,19 +1,15 @@
-angular.module('alurapic').controller('FotosController', function ($scope, $http) {
-    $scope.fotos = [];
-    $scope.filtro = '';
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+	
+	$scope.fotos = [];
+	$scope.filtro = '';
 
-    /*  $http é assincrona */
-    $http.get("v1/fotos").success(function(fotos) {
-        $scope.fotos = fotos;
-    }).error(function(erro) {
-        console.log(erro);
-    })
-    /*
-    var promise = $http.get("v1/fotos");
-    promise.then(function(retorno) {
-        $scope.fotos = retorno.data;
-    }).catch(function(error) {
-        console.log(error);
-    });
-    */
+	$http.get('/v1/fotos')
+	.success(function(retorno) {
+		console.log(retorno);
+		$scope.fotos = retorno; // não precisa fazer retorno.data
+	})
+	.error(function(erro) {
+		console.log(erro);
+	});
+
 });
--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,15 +1,18 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-.config(function($routeProvider, $locationProvider) {
+angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
 
-    $locationProvider.html5Mode(true);
-    
-    $routeProvider.when('/fotos', {
-        templateUrl: 'partials/principal.html',
-        controller: 'FotosController'
-    });
-    $routeProvider.when('/fotos/new', {
-        templateUrl: 'partials/fotos.html'
-    });
+		$locationProvider.html5Mode(true);
 
-    $routeProvider.otherwise({ redirectTo : '/fotos'});
-});
+		$routeProvider.when('/fotos', {
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		});
+
+		$routeProvider.when('/fotos/new', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		});
+
+		$routeProvider.otherwise({redirectTo: '/fotos'});
+
+	});
